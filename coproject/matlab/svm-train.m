6019b0224874562f4e4fc6d38b928f276f798a01
glederrey <gael.lederrey@epfl.ch>
2016-05-29 12:58:29 +0200

    UPD: Matlab code for training

--- coproject/matlab/svm-train.m
+++ coproject/matlab/svm-train.m
@@ -20,45 +20,50 @@ times = zeros(10,3);
 
 %% TRAINING
 
+display('Start training for Linear Kernel');
+
 for i = 1:numel(classes)
-    display(['Start training for class ', num2str(i-1)]);
+    display(['Start training for digit ', num2str(i-1)]);
     idx = strcmp(trainLabel, classes(i)); % Create binary classes for each classifier
     
     display(['Start Linear Kernel']);
     tic;
     SVMModelsLin{i} = fitcsvm(trainSet , idx , 'KernelFunction', 'linear');
     times(i,1) = toc;
-    display(['Finished training for Linear Kernel, Time taken: ', num2str(times(i))]);
+    display(['Finished training for digit ', num2str(i-1), '; Time taken: ', num2str(times(i))]);
     trainingTimeLin = trainingTimeLin + times(i,1);
 end
 
 save('SVMModelsLin.mat', SVMModelsLin);
 save('trainingTimeLin.mat', trainingTimeLin);
+
+display('Start training for Polynomial Kernel');
  
 for i = 1:numel(classes)
-    display(['Start training for class ', num2str(i-1)]);
+    display(['Start training for digit ', num2str(i-1)]);
     idx = strcmp(trainLabel, classes(i)); % Create binary classes for each classifier
-       
-    display(['Start Polynomial Kernel']);
+  
     tic;
     SVMModelsPol{i} = fitcsvm(trainSet , idx , 'KernelFunction', 'polynomial', 'PolynomialOrder', 4);
     times(i,2) = toc;
-    display(['Finished training for Polynomial Kernel, Time taken: ', num2str(times(i))]);
+    display(['Finished training for digit ', num2str(i-1), '; Time taken: ', num2str(times(i))]);
     trainingTimePol = trainingTimePol + times(i,2);
 end
 
 save('SVMModelsPol.mat', SVMModelsPol);
 save('trainingTimePol.mat', trainingTimePol);
 
+display('Start training for RBF Kernel');
+
 for i = 1:numel(classes)
-    display(['Start training for class ', num2str(i-1)]);
+    display(['Start training for digit ', num2str(i-1)]);
     idx = strcmp(trainLabel, classes(i)); % Create binary classes for each classifier
     
     display(['Start RBF Kernel']);
     tic;
     SVMModelsRBF{i} = fitcsvm(trainSet , idx , 'KernelFunction', 'rbf');
     times(i,3) = toc;
-    display(['Finished training for RBF Kernel, Time taken: ', num2str(times(i))]);
+    display(['Finished training for digit ', num2str(i-1), '; Time taken: ', num2str(times(i))]);
     trainingTimeRBF = trainingTimeRBF + times(i,3);    
 end
 
