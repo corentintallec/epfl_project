aafa1aa88ab374984ef366e9c946d1f0e4d92ece
glederrey <gael.lederrey@epfl.ch>
2016-06-07 14:45:55 +0200

    UPD: Finished writing experience + conclusion. Just need to review the whole report

--- coproject/code/results.m
+++ coproject/code/results.m
@@ -10,25 +10,6 @@ norm_linear = linear*diag(1./sum(linear',1));
 
 result_linear = sum(diag(norm_linear))/10
 
-figure()
-imagesc(norm_linear);
-colormap(flipud(gray));
-textStrings = num2str(linear(:),'%0.0f');  %# Create strings from the matrix values
-textStrings = strtrim(cellstr(textStrings));  %# Remove any space padding
-[x,y] = meshgrid(1:10);
-hStrings = text(x(:),y(:),textStrings(:),'HorizontalAlignment','center');
-midValue = mean(get(gca,'CLim'));
-textColors = repmat(norm_linear(:) > midValue,1,3);  
-set(hStrings,{'Color'},num2cell(textColors,2));
-
-set(gca,'XTick',1:10,...                         %# Change the axes tick marks
-        'XTickLabel',{'0','1','2','3','4','5','6','7','8','9'},...  %#   and tick labels
-        'YTick',1:10,...
-        'YTickLabel',{'0','1','2','3','4','5','6','7','8','9'},...
-        'TickLength',[0 0]);
-ylabel('Prediction')
-xlabel('Truth')
-
 pol = [970 0 1 0 0 5 1 1 2 0; 0 1123 2 2 1 0 4 0 3 0; 9 6 999 0 2 0 4 8 4 0; ...
     1 5 3 981 0 6 0 6 5 3; 3 2 3 0 960 0 4 1 1 8; 3 2 2 13 1 859 4 1 5 2; ...
     4 6 2 0 4 6 934 0 2 0; 0 14 8 2 1 0 0 997 0 6; 7 2 1 6 5 6 1 2 942 2; ...
@@ -47,3 +28,22 @@ rbf = [974 0 1 0 0 2 0 1 2 0; 0 1128 3 1 0 1 0 1 1 0; 4 0 1017 0 1 0 0 7 3 0; ..
 norm_rbf = rbf*diag(1./sum(rbf',1));
 
 result_rbf = sum(diag(norm_rbf))/10
+
+figure()
+imagesc(norm_rbf);
+colormap(flipud(gray));
+textStrings = num2str(rbf(:),'%0.0f');  %# Create strings from the matrix values
+textStrings = strtrim(cellstr(textStrings));  %# Remove any space padding
+[x,y] = meshgrid(1:10);
+hStrings = text(x(:),y(:),textStrings(:),'HorizontalAlignment','center');
+midValue = mean(get(gca,'CLim'));
+textColors = repmat(norm_rbf(:) > midValue,1,3);  
+set(hStrings,{'Color'},num2cell(textColors,2));
+
+set(gca,'XTick',1:10,...                         %# Change the axes tick marks
+        'XTickLabel',{'0','1','2','3','4','5','6','7','8','9'},...  %#   and tick labels
+        'YTick',1:10,...
+        'YTickLabel',{'0','1','2','3','4','5','6','7','8','9'},...
+        'TickLength',[0 0]);
+ylabel('Prediction')
+xlabel('Truth')
